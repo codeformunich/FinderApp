95be8e87edce3b2ff807fb7818cf1d6b3ba11e3e
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-08-12 17:24:09 +0200

    correct map URL

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -49,7 +49,7 @@
 
   var map = L.map('map');
 
-  L.tileLayer('http://{s}.tiles.mapbox.com/v3/jancborchardt.j7aheln6/997/256/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/jancborchardt.j7aheln6/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
   }).addTo(map);
