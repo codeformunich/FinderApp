3bea50d80ffd03c9bc0704a787e00dfb116d152d
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-08-12 17:04:11 +0200

    add geolocation request

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,10 +47,13 @@
     }
   });
 
-  var map = L.map('map').setView([51.505, -0.09], 13);
-  L.tileLayer('http://{s}.tiles.mapbox.com/v3/MapID/{z}/{x}/{y}.png', {
+  var map = L.map('map');
+
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/MapID/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
   }).addTo(map);
+
+  map.locate({setView: true, maxZoom: 16});
 
 })();
