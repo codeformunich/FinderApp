b8a273451c7f81cc82a9d9ca2f46ee30b4de63d5
BastiRehm <rehmsebastian@gmail.com>
2015-02-11 21:36:02 +1000

    New about page added

--- app/scripts/ampersand/about-view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var AmpersandView = require('ampersand-view');
-var template = require('./templates/about');
-
-module.exports = AmpersandView.extend({
-  autoRender: true,
-  template: template,
-});
